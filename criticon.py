dea7fdadf848960a2b8d620dc684b0f4adf0b9f5
decolector <cmart@decolector.net>
2016-07-20 19:09:20 +0000

    criticon first beta version

---
+++ criticon.py
@@ -0,0 +1,104 @@
+#coding:utf-8
+#!/usr/bin/python
+
+## taken from https://gist.github.com/loleg/5b581d774fc8500325f7
+import random
+import time
+import sys
+import json
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import cv2
+import zbar
+import festival
+from PIL import Image
+
+# Debug mode
+DEBUG = True
+if len(sys.argv) > 1:
+    DEBUG = sys.argv[-1] == 'DEBUG'
+
+# Configuration options
+FULLSCREEN = not DEBUG
+if not DEBUG:
+    RESOLUTION = (640, 480)
+else:
+    RESOLUTION = (480, 270)
+
+json_file = open("data/db.json")
+
+db = json.load(json_file)
+
+# Initialise Raspberry Pi camera
+camera = PiCamera()
+camera.resolution = RESOLUTION
+#camera.framerate = 10
+#camera.vflip = True
+#camera.hflip = True
+#camera.color_effects = (128, 128)
+# set up stream buffer
+rawCapture = PiRGBArray(camera, size=RESOLUTION)
+# allow camera to warm up
+time.sleep(0.1)
+print "PiCamera ready"
+
+
+# load json data file
+jf = open("data/db.json")
+db = json.load(jf)
+
+festival.execCommand("(voice_el_diphone)")
+
+# Initialise OpenCV window
+if FULLSCREEN:
+    cv2.namedWindow("#iothack15", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("#iothack15", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+else:
+    cv2.namedWindow("#iothack15")
+
+print "OpenCV version: %s" % (cv2.__version__)
+print "Press q to exit ..."
+
+scanner = zbar.ImageScanner()
+scanner.parse_config('enable')
+
+# Capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # as raw NumPy array
+    output = frame.array.copy()
+
+    # raw detection code
+    gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY, dstCn=0)
+    pil = Image.fromarray(gray)
+    width, height = pil.size
+    raw = pil.tobytes()
+
+    # create a reader
+    image = zbar.Image(width, height, 'Y800', raw)
+    scanner.scan(image)
+
+    # extract results
+    for symbol in image:
+        # do something useful with results
+        streeng = "decoded " + str(symbol.type) + " symbol " + str(symbol.data)
+        print streeng
+        tags = db['codes'][str(symbol.data)]
+        text = random.choice(db[tags[0]]) + random.choice(db[tags[1]])
+        print(text)
+        festival.sayText(text)
+
+
+    # show the frame
+    cv2.imshow("#iothack15", output)
+
+    # clear stream for next frame
+    rawCapture.truncate(0)
+
+    # Wait for the magic key
+    keypress = cv2.waitKey(1) & 0xFF
+    if keypress == ord('q'):
+        break
+
+# When everything is done, release the capture
+camera.close()
+cv2.destroyAllWindows()
